fe6b6e28773ad41543c831edea0f2aebf90ddb82
Sobirov3636 <sobirov3636@gmail.com>
2024-02-04 16:15:14 +0900

    EJS framework orqali ananaviy frontend qurish

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ app.set("views", "views"); // folderni korsatyapmiz.
 app.set("view engine", "ejs"); // view engine ejs ekanligini korsatyapmiz.
 
 // 4: Routing code
-app.get("/hello", function (req, res) {
-  res.end("<h1>HELLO WORLD</h1>");
+app.post("/create-item", (req, res) => {
+  console.log(req);
+  res.json({ test: "success" });
 });
-app.get("/gift", function (req, res) {
-  res.end("<h1>Siz sovg'alar bo'limidasiz</h1>");
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
