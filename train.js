a5e61b26311a1753f8849b6bc805a4659dfad0b7
Sobirov3636 <sobirov3636@gmail.com>
2024-02-13 00:49:38 +0900

    feat: Task C solution

--- train.js
+++ train.js
@@ -1,51 +1,103 @@
-// Count Animals
-const animal_list = [
-  "fox",
-  "ant",
-  "bird",
-  "lion",
-  "wolf",
-  "deer",
-  "bear",
-  "frog",
-  "hen",
-  "mole",
-  "duck",
-  "goat",
-  "dog",
-  "cat",
-  "bat",
-  "cock",
-  "cow",
-];
+// C-TASK
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 
-const findAnimals = (str) => {
-  return animal_list.filter((ele) => {
-    for (const char of ele) {
-      if (!str.includes(char)) {
-        return false;
-      }
+class Shop {
+  constructor(product1, product2, product3) {
+    this.non = product1;
+    this.lagmon = product2;
+    this.cola = product3;
+  }
+
+  // methods
+
+  getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const currentTime = `${hours}:${minutes}`;
+    return currentTime;
+  }
+
+  qoldiq() {
+    const currentTime = this.getCurrentTime();
+    console.log(`Hozir ${currentTime}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`);
+  }
+  sotish(productName, num) {
+    if (productName === "non") {
+      this.non -= num;
+    } else if (productName === "lagmon") {
+      this.lagmon -= num;
+    } else {
+      this.cola -= num;
     }
-    return true;
-  });
-};
+  }
+  qabul(productName, num) {
+    if (productName === "non") {
+      this.non += num;
+    } else if (productName === "lagmon") {
+      this.lagmon += num;
+    } else {
+      this.cola += num;
+    }
+  }
+}
 
-const javob = findAnimals("dgoat");
-console.log(javob);
+const shop = new Shop(4, 5, 2);
+shop.qabul("cola", 5);
+shop.sotish("cola", 1);
+shop.sotish("lagmon", 2);
+shop.sotish("non", 3);
+shop.qoldiq();
+
+// // Count Animals
+// const animal_list = [
+//   "fox",
+//   "ant",
+//   "bird",
+//   "lion",
+//   "wolf",
+//   "deer",
+//   "bear",
+//   "frog",
+//   "hen",
+//   "mole",
+//   "duck",
+//   "goat",
+//   "dog",
+//   "cat",
+//   "bat",
+//   "cock",
+//   "cow",
+// ];
+
+// const findAnimals = (str) => {
+//   return animal_list.filter((ele) => {
+//     for (const char of ele) {
+//       if (!str.includes(char)) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   });
+// };
+
+// const javob = findAnimals("dgoat");
+// console.log(javob);
 
 // B-TASK
-const countLetter = (string) => {
-  let count = 0;
-  string.split("").forEach((letter) => {
-    if (parseInt(letter)) {
-      count++;
-    }
-  });
-  return count;
-};
+// const countLetter = (string) => {
+//   let count = 0;
+//   string.split("").forEach((letter) => {
+//     if (parseInt(letter)) {
+//       count++;
+//     }
+//   });
+//   return count;
+// };
 
-const res = countLetter("enginas2345eer");
-console.log(`Berilgan string ichida ${res} ta raqam bor`);
+// const res = countLetter("enginas2345eer");
+// console.log(`Berilgan string ichida ${res} ta raqam bor`);
 // // A-TASK
 // const countLetter = (string, l) => {
 //   let count = 0;
